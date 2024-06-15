999f12364722568c48cbb13cec8cb539bb65a8ab
darshan45672 <drshnbhandary@gmail.com>
2024-06-15 08:57:55 +0530

    revamped directory and created new superuser

--- ecom/settings.py
+++ ecom/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-25gs=c0px0bhyk^9h44*vb++n5-oryy(=f#$u3!)*(3li6_n-n'
+SECRET_KEY = 'django-insecure-cfh4=et0ct7p1i4#b05y3-+qd5^l#7#02r0@v#-&9%p(&rfg3%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
