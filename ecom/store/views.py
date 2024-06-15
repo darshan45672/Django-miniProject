0e1369721f39893e16f9bed339a23698b2ad3670
darshan45672 <drshnbhandary@gmail.com>
2024-06-15 13:17:25 +0530

    implemented component based page design and added routing to nav bar

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -4,3 +4,6 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products' : products})
+
+def about(request):
+    return render(request, 'about.html', {})
