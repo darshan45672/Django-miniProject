ebc4ae194dbb80a783cd7834df346d71cb2354ca
darshan45672 <drshnbhandary@gmail.com>
2024-06-17 12:18:09 +0530

    implemented customer sessions Shopping cart

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -11,3 +11,13 @@ class Cart():
         
         # making cart available in very page
         self.cart = cart
+    
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        
+        self.session.modified = True
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cartSummary(request):
     return render(request, "cartSummary.html", {})
 
 def cartAdd(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        response = JsonResponse({"Product Name: ": product.name})
+
+        return response    
 
 def cartDelete(request):
     pass
