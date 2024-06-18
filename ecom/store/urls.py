355599d0f4ff747a6dc1be45d58ea868b5b222bc
darshan45672 <drshnbhandary@gmail.com>
2024-06-18 23:31:58 +0530

    implemented show categories

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/',views.registerUser, name="register"),
     path('product/<int:pk>',views.product, name="product"),
     path('category/<str:categorySearch>',views.category, name="category"),
+    path('category-summary/',views.categorySummary, name="categorySummary"),
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
-# from django import forms
+
+def categorySummary(request):
+    categories = Category.objects.all()
+    return render(request, 'categorySummary.html', {"categories": categories})
 
 def home(request):
     products = Product.objects.all()
