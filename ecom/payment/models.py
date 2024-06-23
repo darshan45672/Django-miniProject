894ad24f5cf8163152033275ba8c4499e3ff5e14
darshan45672 <drshnbhandary@gmail.com>
2024-06-24 00:06:48 +0530

    updated model

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    fullName = models.CharField(max_length=200, null=False, blank=False)
-    email = models.CharField(max_length=200, null=False, blank=False)
-    address1 = models.CharField(max_length=200, null=False, blank=False)
-    address2 = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200, null=False, blank=False)
-    state = models.CharField(max_length=200, null=False, blank=False)
-    zipcode = models.CharField(max_length=200, null=False, blank=False)
-    country = models.CharField(max_length=200, null=False, blank=False)
+    shippingFullName = models.CharField(max_length=200, null=False, blank=False)
+    shippingEmail = models.CharField(max_length=200, null=False, blank=False)
+    shippingAddress1 = models.CharField(max_length=200, null=False, blank=False)
+    shippingAddress2 = models.CharField(max_length=200, null=True, blank=True)
+    shippingCity = models.CharField(max_length=200, null=False, blank=False)
+    shippingState = models.CharField(max_length=200, null=False, blank=False)
+    shippingZipcode = models.CharField(max_length=200, null=False, blank=False)
+    shippingCountry = models.CharField(max_length=200, null=False, blank=False)
 
     class Meta:
         verbose_name_plural = "Shipping Address"
